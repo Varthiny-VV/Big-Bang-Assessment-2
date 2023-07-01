a68ef276e9859543313aaf8ff919ca1c63927de2
Varthiny <vishnuvarthiny@gmail.com>
2023-07-01 19:06:09 +0530

    FrontEnd folder created

--- Backend/BigBangAssessment@2/Services/DoctorRepo.cs
+++ Backend/BigBangAssessment@2/Services/DoctorRepo.cs
@@ -1,5 +1,6 @@
 ﻿using BigBangAssessment_2.Interfaces;
 using BigBangAssessment_2.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BigBangAssessment_2.Services
 {
@@ -33,6 +34,47 @@ namespace BigBangAssessment_2.Services
         {
             var doctor = _context.Doctors.FirstOrDefault(d => d.Name == key);
             return doctor;
+        }
+
+        public async Task<Doctor?> Delete(string name)
+        {
+            var doctor = await Get(name);
+            if (doctor != null)
+            {
+                _context.Doctors.Remove(doctor);
+                await _context.SaveChangesAsync();
+                return doctor;
+            }
+            return null;
+        }
+
+        public async Task<ICollection<Doctor>?> GetAll()
+        {
+            var doctors = await _context.Doctors.Include(e => e.User).Include(e => e.Speciality).ToListAsync();
+            if (doctors.Count > 0)
+                return doctors;
+            return null;
+        }
+
+        public async Task<Doctor?> Update(Doctor item)
+        {
+            var doctor = await Get(item.Name);
+            if (doctor != null)
+            {
+                doctor.Id = item.Id;
+                doctor.Name = item.Name;
+                doctor.Gender = item.Gender;
+                doctor.Age = item.Age;
+                doctor.PhoneNumber = item.PhoneNumber;
+                doctor.Speciality = item.Speciality;
+                doctor.Experience = item.Experience;
+                doctor.ConsultationFee = item.ConsultationFee;
+                doctor.status = item.status;
+                doctor.PatientList = item.PatientList;
+                await _context.SaveChangesAsync();
+                return doctor;
+            }
+            return null;
         }
     }
 }
